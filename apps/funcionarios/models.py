d3a53160213a6bbdd273676c225adefba6ca3a49
vladimirmaciel <vladimirmaciel@gmail.com>
2022-06-11 21:47:40 -0300

    Apps funcionarios

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+
+class Funcionairo(models.Model):
+    nome = models.CharField(max_length=100)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def  __str__(self):
+        return self.nome
