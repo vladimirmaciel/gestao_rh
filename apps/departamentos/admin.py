9fa98146d91bc98f2958d8bfd300cc10766082f9
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-12-14 01:43:35 -0300

    registrando em seus respectivos admin.py

--- apps/departamentos/admin.py
+++ apps/departamentos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.departamentos.models import Departamento
+
 # Register your models here.
+admin.site.register(Departamento)
--- apps/funcionarios/admin.py
+++ apps/funcionarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from apps.funcionarios.models import Funcionario
+
+admin.site.register(Funcionario)
 # Register your models here.
