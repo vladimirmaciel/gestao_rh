3212c10f525c076e46134082059d4db83b4b803b
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-12-15 17:49:00 -0300

    feat(edittar-empresa): Editar empresa
    View para editar empresa

--- apps/empresas/urls.py
+++ apps/empresas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EmpresaCreateView
+from .views import EmpresaCreateView, EmpresaEditView
 
 app_name = 'empresas'
 
 urlpatterns = [
     path('create/', EmpresaCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', EmpresaEditView.as_view(), name='update'),
 ]
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,11 +1,14 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.generic import CreateView
+from django.http import HttpResponse, request
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
 from .models import Empresa
-from ..funcionarios.models import Funcionario
 
 
-class EmpresaCreateView(LoginRequiredMixin,CreateView):
+class EmpresaCreateView(LoginRequiredMixin, CreateView):
     model = Empresa
     fields = ['nome']  # Exemplo de campos
     template_name = 'empresas/empresa_form.html'
@@ -17,3 +20,17 @@ class EmpresaCreateView(LoginRequiredMixin,CreateView):
         funcionario.empresa = obj_empresa
         funcionario.save()
         return HttpResponse('Empresa criada com sucesso!')  # Exemplo de retorno
+
+
+class EmpresaEditView(LoginRequiredMixin, UpdateView):
+    model = Empresa
+    fields = ['nome']  # Exemplo de campos
+    template_name = 'empresas/empresa_form.html'
+    success_url = reverse_lazy('core:home')  # Redirecionar após edição# Exemplo de redirecionamento
+
+    def form_valid(self, form):
+        # Adiciona uma mensagem de sucesso
+        messages.success(self.request, 'A empresa foi alterada com sucesso!')
+        return super().form_valid(form)
+
+
