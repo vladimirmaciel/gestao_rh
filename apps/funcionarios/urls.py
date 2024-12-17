e9530f26b090a157267aee6bad8d081255302757
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-12-17 01:32:46 -0300

    feat(gerenciar-funcionarios): Gerenciar funcionario
    Adcionado validadores ao criar e remover funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import home, FuncionarioCreate, ListFuncionariosView, FuncionarioUpdateView
+from .views import home, FuncionarioCreate, ListFuncionariosView, FuncionarioUpdateView, FuncionarioDeleteView
 
 app_name = 'funcionarios'
 urlpatterns = [
     path('', ListFuncionariosView.as_view(), name='list'),
     path('create/', FuncionarioCreate.as_view(), name='create'),
     path('update/<int:pk>/', FuncionarioUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', FuncionarioDeleteView.as_view(), name='delete'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,8 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from apps.funcionarios.models import Funcionario
 
@@ -11,25 +13,58 @@ def home(request):
     return HttpResponse('Olá mundo!')
 
 
-class FuncionarioCreate(LoginRequiredMixin,CreateView):
+class FuncionarioCreate(LoginRequiredMixin, CreateView):
     model = Funcionario
-    fields = ['nome', 'departamentos', 'empresa']
+    fields = ['nome', 'departamentos']
     template_name = 'funcionarios/funcionario_form.html'
     success_url = reverse_lazy('funcionarios:list')
 
-    def get_success_url(self):
-        return super().get_success_url()
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        funcionario.empresa = self.request.user.funcionario_user.empresa
+        funcionario.user = self.create_user_for_funcionario(funcionario)
+        funcionario.save()
+        return super().form_valid(form)
 
-class ListFuncionariosView(LoginRequiredMixin,ListView):
+
+    def create_user_for_funcionario(self, funcionario):
+      nome_parts = funcionario.nome.split()
+      username = ''.join(nome_parts[:2])
+      return User.objects.create_user(username=username)
+
+
+class ListFuncionariosView(LoginRequiredMixin, ListView):
     model = Funcionario
 
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario_user.empresa
         return Funcionario.objects.filter(empresa=empresa_logada)
 
-class FuncionarioUpdateView(UpdateView):
+
+class FuncionarioUpdateView(LoginRequiredMixin, UpdateView):
     model = Funcionario
     fields = ["nome", "departamentos", "empresa"]
     template_name = "funcionarios/funcionario_update_form.html"
     success_url = reverse_lazy('funcionarios:list')
     template_name_suffix = "_update_form"
+
+
+class FuncionarioDeleteView(LoginRequiredMixin, DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('funcionarios:list')
+    template_name = "funcionarios/funcionario_confirm_delete.html"
+
+
+    def form_valid(self, form):
+        if self.verificar_se_pode_deletar():
+            messages.success(self.request, 'O funcionário foi deletado com sucesso!')
+            return super().form_valid(form)
+        else:
+            return HttpResponse('Funcionário não pode ser excluidos', status=403)
+
+    def verificar_se_pode_deletar(self):
+        funcionario = self.get_object()
+        usuario_logado = self.request.user.funcionario_user
+        if funcionario.empresa == usuario_logado.empresa and funcionario != usuario_logado:
+            return True
+        return False
