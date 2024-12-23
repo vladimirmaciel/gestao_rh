474fc5d911c609a2a79812582cf8c74ade50cb63
Vlamidir Serra <vladimirmaciel@gmail.com>
2024-12-23 00:02:58 -0300

    feat(deletar-departamento): deletar departamento
    Criardo view, url e template , relacioandos a deletar departamento

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import DepartamentoListView, CreateDepartamentoView
+from .views import DepartamentoListView, CreateDepartamentoView, UpateDepartamentoView, DeleteDepartamentoView
 
 app_name = 'departamentos'
 
 urlpatterns = [
     path('', DepartamentoListView.as_view(), name='list'),
     path('create/', CreateDepartamentoView.as_view(), name='create'),
+    path('update/<int:pk>/', UpateDepartamentoView.as_view(), name='update'),
+    path('delete/<int:pk>/', DeleteDepartamentoView.as_view(), name='delete'),
 ]
 
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from apps.departamentos.models import Departamento
-from apps.empresas.models import Empresa
 
 
 # Create your views here.
@@ -21,23 +19,46 @@ class DepartamentoListView(LoginRequiredMixin, ListView):
 
 class CreateDepartamentoView(LoginRequiredMixin, CreateView):
     model = Departamento
-    fields = ['nome', 'empresa', 'is_active']
+    fields = ['nome']
     template_name = 'departamentos/departamento_form.html'
     success_url = '/departamentos/'
 
-    def get_form(self, form_class=None):
-        # Obtém o formulário padrão
-        form = super().get_form(form_class)
-
-        # Filtra o campo 'empresa' para mostrar apenas a empresa do usuário logado
-        funcionario = self.request.user.funcionario_user
-        form.fields['empresa'].queryset = Empresa.objects.filter(id=funcionario.empresa.id)
-
-        return form
+    # def get_form(self, form_class=None):
+    #     # Obtém o formulário padrão
+    #     form = super().get_form(form_class)
+    #
+    #     # Filtra o campo 'empresa' para mostrar apenas a empresa do usuário logado
+    #     funcionario = self.request.user.funcionario_user
+    #     form.fields['empresa'].queryset = Empresa.objects.filter(id=funcionario.empresa.id)
+    #
+    #     return form
 
     def form_valid(self, form):
-        obj_departamento = form.save()
-        # funcionario = self.request.user.funcionario_user
-        # funcionario.departamentos.add(obj_departamento)
-        # funcionario.save()
-        return HttpResponse('Departamento criado com sucesso!')  # Exemplo de retorno
+        obj_departamento = form.save(commit=False)
+        obj_departamento.empresa = self.request.user.funcionario_user.empresa
+        obj_departamento.save()
+        return super(CreateDepartamentoView, self).form_valid(form)
+
+
+class UpateDepartamentoView(LoginRequiredMixin, UpdateView):
+    model = Departamento
+    fields = ['nome','is_active']
+    template_name = 'departamentos/departamento_update_form.html'
+    success_url = '/departamentos/'
+
+    def form_valid(self, form):
+        obj_departamento = form.save(commit=False)
+        obj_departamento.empresa = self.request.user.funcionario_user.empresa
+        obj_departamento.save()
+        return super(UpateDepartamentoView, self).form_valid(form)
+
+class DeleteDepartamentoView(LoginRequiredMixin, DeleteView):
+    model = Departamento
+    template_name = 'departamentos/departamento_delete_confirm.html'
+    success_url = '/departamentos/'
+
+    def delete(self, request, *args, **kwargs):
+        obj = self.get_object()
+        obj.is_active = False
+        obj.save()
+        return super(DeleteDepartamentoView, self).delete(request, *args, **kwargs)
